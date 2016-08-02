e27d51f2ba15161ad11cb87b03493f36873a3b28
200915137 <jonatansmns@gmail.com>
2016-08-02 07:05:12 -0600

    CrearDisco

--- Proyecto1_Fase1_200915137/main.c
+++ Proyecto1_Fase1_200915137/main.c
@@ -17,17 +17,81 @@ struct struc_disco{
     char puntero[9];
 };
 
+void menu();
 void comandos();
 void crear_disco();
 
 int main()
 {
-     comandos();
+    char c;
+
+   printf("Enter character: ");
+   c = getchar();
+
+   printf("Character entered: ");
+   putchar(c);
+
+    //menu();
+     //comandos();
     return 0;
 }
 
+void menu()
+{
+   int opciones;
+    FILE *f;
+
+    do{
+        char codigo[10];
+        char nombre[50];
+        char telefono[12];//+502 4285-5821
+
+        char palabra[30];
+
+
+        printf("MENU PRINCIPAL\n\n");
+        printf("1. Crear disco\n");
+        printf("2. Buscar\n");
+        printf("3. Modificar\n");
+        printf("4. Eliminar\n");
+        printf("5. Salir\n");
+        printf("Elija opcion: ");
+        scanf("%d", &opciones);
+        switch(opciones)
+        {
+            case 1:
+                crear_disco();
+            break;
+            case 2:
+
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            default:
+            printf("Error opcion incorrecta\n\n");
+            break;
+        }
+    }while(opciones != 5);
+
+}
+
 void crear_disco(){
-printf("Se creo un disco duro");
+    struct struc_disco tdisco;
+
+    printf("Ingresar el nombre del disco \n");
+    scanf("%s", tdisco.nombre);
+    printf("Ingrese el tamanio del disco");
+    int tamanio=0;
+    int repetir=0;
+
+    while()
+    {
+
+    }
 
 }
 
