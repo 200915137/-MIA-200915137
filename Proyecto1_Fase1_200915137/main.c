f07be5693cacde248f8de827dfa7152ee0f17a4f
200915137 <jonatansmns@gmail.com>
2016-08-03 23:50:18 -0600

    fase 1

--- Proyecto1_Fase1_200915137/main.c
+++ Proyecto1_Fase1_200915137/main.c
@@ -17,31 +17,33 @@ struct struc_disco{
     char puntero[9];
 };
 
-//guarda la ruta de los discos duros del sistema
-char sistema[8]="sistema/";
+
 //bit utilizado para guardar 0's
 char buffer[1];
 //contador de fors
 int ifor=0;
-
 //direccion del disco actual
 char directorio[58];
+//extension de Disco ".sdk"
+char extension_disco[4]=".dsk";
+
 
 void menu();
 void comandos();
 void crear_disco();
+int escribeInfoDirectorio(char *nombreFichero);
 void tipo_de_tamanio();
 int main()
 {
-
 
     menu();
      //comandos();
     return 0;
 }
 
-void set_disco(char nombre[50]){
-    sprintf(directorio, "%s%s", sistema, nombre);
+void set_disco(char ruta_carpeta[52],char nombre[52]){
+    sprintf(directorio, "%s%s%s",ruta_carpeta, nombre,extension_disco);
+
 }
 
 void menu()
@@ -57,9 +59,9 @@ void menu()
 
         printf("MENU PRINCIPAL\n\n");
         printf("1. Crear disco\n");
-        printf("2. Buscar\n");
-        printf("3. Modificar\n");
-        printf("4. Eliminar\n");
+        printf("2. Eliminar Disco\n");
+        printf("3. Montar Disco\n");
+        printf("4. Desmontar\n");
         printf("5. Salir\n");
         printf("Elija opcion: ");
         scanf("%d", &opciones);
@@ -87,6 +89,7 @@ void menu()
 
 void crear_disco(){
     struct struc_disco tdisco;
+    char ruta[52];
     int opt=0;
     printf("Ingresar el nombre del disco. \n");
     scanf("%s", tdisco.nombre);
@@ -125,25 +128,95 @@ void crear_disco(){
 
     }
 
-    //printf("Ingrese la ruta de destino...\n");
-    //scanf("%s",&directorio);
-    //directorio=(char) ;
+    printf("Ingrese la ruta de destino...\n");
+    scanf("%s",&ruta);
     sprintf(tdisco.tamanio, "%d", tamanio);
     sprintf(tdisco.particion, "%d", 0);
     sprintf(tdisco.puntero, "%d", (int)sizeof(tdisco));
     printf("Creando disco...\n");
 
+    if(escribeInfoDirectorio(ruta))
+    {
+        printf("se encontro el directorio %s\n", ruta);
+    }
+    else
+    {
+        /*
+         * Se crea un directorio DIRECTORIO con permisos 777 y se comprueba.
+         * 4+2+1=7 es decir puedo leer,escribir y ejecutar
+         */
+        mkdir (ruta, 0777);
+        //escribeInfoDirectorio(ruta);
+    }
 
     //creando el disco
-    set_disco(tdisco.nombre);
+    set_disco(ruta,tdisco.nombre);
     FILE *f = fopen (directorio, "w+b");
-    for(ifor=0;ifor<tamanio*1024;ifor++)
-        fwrite (buffer, sizeof(buffer), 1024, f);
+
+    if(opt==1)
+    {
+        for(ifor=0;ifor<tamanio;ifor++)
+            fwrite (buffer, sizeof(buffer), 1024, f);
+    }
+
+    if(opt==2 || opt==3)
+    {
+        for(ifor=0;ifor<tamanio*1024;ifor++)
+            fwrite (buffer, sizeof(buffer), 1024, f);
+    }
+
     //guardando informacion del disco
     rewind(f);
     fwrite(&tdisco,sizeof(tdisco),1,f);
     fclose(f);
-    printf("Disco %s creado exitosamente\n", tdisco.nombre);
+    printf("Disco %s fue creado exitosamente ", tdisco.nombre);
+    printf("en %s\n\n", directorio);
+}
+
+
+int escribeInfoDirectorio(char *nombreFichero)
+{
+
+    /*
+     * Estructura para recoger la información del fichero.
+     */
+    struct stat datosFichero;
+
+    /*
+     * Comprobación del parámetro de entrada
+     */
+    if (nombreFichero == NULL)
+        return;
+
+    /*
+     * Empezamos a escribir el resultado de la comprobación.
+     */
+    printf ("El fichero %s --> ", nombreFichero);
+
+    /*
+     * Se comprueba si existe el fichero.
+     * ¡Atención!. En esta llamada a lstat() se rellena datosFichero,
+     * y por ello se puede usar datosFichero->st_mode en los siguientes if.
+     */
+    if (lstat (nombreFichero, &datosFichero) == -1)
+    {
+        printf ("no existe\n");
+        return 0;
+    }
+    else
+    {
+        /* Se comprueba si es un directorio */
+        if (S_ISDIR(datosFichero.st_mode)){
+            printf ("es un directorio\n");
+            return 1;
+        }
+
+        /* Es otra cosa */
+        else
+            printf ("no es fichero, directorio, link ni link simbólico\n");
+
+
+    }
 }
 
 void tipo_de_tamanio()
